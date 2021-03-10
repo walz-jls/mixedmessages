161be587494c3b82208d4b61923f0c22e4a91407
walz-jls <jules.caquard@orange.fr>
2021-03-10 15:34:36 +0100

    Create first data arrays

---
+++ functions.js
@@ -0,0 +1,7 @@
+//Data storing
+const instructionType = ['hdg','level','speed','shoot'];
+const turnDir = [left,right];
+const frequencies = [122.415,118.430,124.830,128.765,135.115];
+
+
+//function to get type of instruction
