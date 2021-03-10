8b54c9adcc0a61dfa07860a4d106e50fd4f91918
walz-jls <jules.caquard@orange.fr>
2021-03-10 18:13:22 +0100

    First template messages

--- functions.js
+++ functions.js
@@ -5,3 +5,13 @@ const frequencies = [122.415,118.430,124.830,128.765,135.115];
 
 
 //function to get type of instruction
+
+
+//message template
+function headingTemplate(hdg,direction){
+  return `turn ${direction} heading ${hdg}`
+};
+
+function climbTemplate(FL,tendancy){
+  return `${tendancy} flight level ${FL}`
+};
