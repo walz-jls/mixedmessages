9416871b3cea5d7ab118df5dc847b8d18835e12e
walz-jls <jules.caquard@orange.fr>
2021-03-11 11:38:34 +0100

    Finish clr templates, add object factory

--- functions.js
+++ functions.js
@@ -1,13 +1,49 @@
 //Data storing
 const instructionType = ['hdg','level','speed','shoot'];
-const turnDir = [left,right];
-const frequencies = [122.415,118.430,124.830,128.765,135.115];
+const turnDir = ['left','right'];
+const frequencies = [['Bordeaux',122.415],['Bordeaux',118.430],['Bordeaux',135.115],['Brest',134.240],['Paris',132.265],['Madrid',127.830],['Marseille',132.365]];
+const possibleTendancy = ['+','=','-'];
 
 
 //function to get type of instruction
+function instType(){
+  let nombreClairance = Math.floor(Math.random()*3 + 1)
+  let types = Array.from(instructionType)
+  let finaltype = []
+  do {
+    let i = Math.floor(Math.random()*types.length)
+    finaltype.push(types[i])
+    types.splice(i,1)
+    nombreClairance -= 1
+  } while (nombreClairance > 0)
+  return finaltype
+};
 
+//defining the instruction object
+function clairanceFactory(hdg,level,speed,nextfreq) {
+  return {
+    hdg,
+    level,
+    speed,
+    nextfreq,
+    fleshOut(){
+      if (this.hdg !== 0){
+        this.turndir = possibleTendancy[Math.floor(Math.random()*3)]
+      }
+      if (this.level !== 0) {
+        this.vertTendancy = possibleTendancy[Math.floor(Math.random()*3)]
+      }
+      if (this.speed !== 0) {
+        this.speedTendancy = possibleTendancy[Math.floor(Math.random()*3)]
+      }
+      if (this.nextFreq === true) {
+        this.speedTendancy = frequencies[Math.floor(Math.random()*frequencies.length)]
+      }
+    }
+  }
+};
 
-//message template
+//Instructions templates
 function headingTemplate(hdg,direction){
   return `turn ${direction} heading ${hdg}`
 };
@@ -15,3 +51,34 @@ function headingTemplate(hdg,direction){
 function climbTemplate(FL,tendancy){
   return `${tendancy} flight level ${FL}`
 };
+
+function machTemplate(machNb,tendancy){
+  switch (tendancy){
+    case '+':
+      return `Keep Mach number ${machNb} minimum`;
+      break;
+    case '-':
+      return `Fly Mach number ${machNb} or less`;
+      break;
+    default :
+      return `Keep ${machNb} exactly`;
+  }
+};
+
+function shootTemplate(frequency){
+  return `call ${frequency[0]}, ${frequency[1]}, bye bye`
+};
+
+//function to join the instrusctions into a single message
+function transmit(callsign,instructions){ //callsign is a string, instructions an array
+  let transmission = callsign + instructions.join(',') + '.'
+  console.log(transmission)
+};
+
+console.log(instType());
+let clr = clairanceFactory(180,340,.75,['Bordeaux',122.415]);
+//let clr2 = clairanceFactory(hdg = 180,level =340);
+console.log(clr);
+clr.fleshOut();
+console.log(clr);
+//console.log(clr2);
